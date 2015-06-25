f070f288c5d765581d3bb1b8ad47bba912d6ec19
Josh Higgins <jshhggns@gmail.com>
2015-06-25 17:17:42 -0400

    Serializers first attempt

--- atus_api/api/serializer.py
+++ atus_api/api/serializer.py
@@ -1,1 +1,25 @@
 __author__ = 'joshuahiggins'
+from rest_framework import serializers
+from .models import Respondent, HouseholdMember, ActivityCodes, ActivityInstances
+
+class RespondentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Respondent
+
+
+class HouseholdMemberSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = HouseholdMember
+
+
+class ActivityCodesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ActivityCodes
+
+
+class ActivityInstancesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ActivityInstances
+
+
+
